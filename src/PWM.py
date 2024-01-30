566cb23a55b43e25e326cbac639002a32c501d6b
JW <156856748+JWW22@users.noreply.github.com>
2024-01-30 11:01:05 -0800

    Restructuring Files

---
+++ src/PWM.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    pinPA0 = pyb.Pin(pyb.Pin.board.PA0, pyb.Pin.OUT_PP) # Initialize pin PA0
+    tim2 = pyb.Timer(2, freq=20000)
+    ch1 = tim2.channel(1, pyb.Timer.PWM, pin=pinPA0)
+    ch1.pulse_width_percent(50)
+#     pinPA0.value(0)
+    # brendan comment for git take two
+    #gggggg
